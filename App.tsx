47c090050eee72ecf4f6a8a1ea166fb51e13a5c0
Timothee68 <lucarellitim@hotmail.fr>
2023-09-19 15:33:31 +0200

    init

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {  createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Article from "./screens/Article";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+                screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName:string = "";
+                  if (route.name === "Home") {
+                    iconName = focused ? "home" : "home-outline";
+                  }
+                  else if (route.name === "Login") {
+                    iconName = focused ? "person-circle" : "person-circle-outline";
+                  } else if (route.name === "Article") {
+                    iconName = focused ? "newspaper" : "newspaper-outline";
+                  }
+                  return <Ionicons name={iconName as any } size={size} color={color} />;
+                },
+                tabBarActiveTintColor: "blue",
+                tabBarInactiveTintColor: "gray",
+              })}>
+
+        <Tab.Screen name="Home" component={Home} options={{ unmountOnBlur: true }} />
+        <Tab.Screen name="Article" component={Article} />
+        <Tab.Screen name="Login" component={Login} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Article.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Article() {
+    return <Text>Article page</Text>;
+}

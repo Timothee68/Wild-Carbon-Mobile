6978e47516c1deb5e84ec78e49b4b07af59f848f
Charlotte Kieffer <kindred242@gmail.Com>
2023-09-21 11:57:49 +0200

    added logo in header plus style

--- screens/MyTabs.tsx
+++ screens/MyTabs.tsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { TouchableOpacity, Image, StyleSheet } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 import Article from "./Article";
@@ -21,7 +22,31 @@ export default function MyTabs() {
 					}
 					return <Ionicons name={iconName as any} size={size} color={color} />;
 				},
-				tabBarActiveTintColor: "#7ED957",
+				headerStyle: {
+					backgroundColor: "#D7CBB5",
+				},
+				headerTitle: () => (
+					<Image
+						source={require("./assets/leaf.png")}
+						style={styles.logoCentral}
+					/>
+				),
+				headerTitleAlign: "center",
+				headerRight: () => (
+					<TouchableOpacity
+						onPress={() => {
+							// Naviguez vers l'écran de profil ici
+						}}
+						style={styles.logoProfil}
+					>
+						<Ionicons
+							size={30}
+							color={"#3C8962"}
+							name="person-sharp"
+						></Ionicons>
+					</TouchableOpacity>
+				),
+				tabBarActiveTintColor: "blue",
 				tabBarInactiveTintColor: "gray",
 			})}
 		>
@@ -35,3 +60,29 @@ export default function MyTabs() {
 		</Tab.Navigator>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+	},
+	title: {
+		textAlign: "center",
+		backgroundColor: "#D644DB",
+		fontWeight: "bold",
+		fontSize: 15,
+	},
+	logoHeader: {
+		width: 40,
+		height: 40,
+		marginLeft: 20,
+	},
+	logoProfil: {
+		marginRight: 20,
+	},
+	logoCentral: {
+		width: 90,
+		height: 60,
+		marginBottom: 15,
+	},
+});

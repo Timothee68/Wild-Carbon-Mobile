62382517705f0d292b2d28725dc936b4369dab18
Timothee68 <lucarellitim@hotmail.fr>
2023-09-22 16:58:45 +0200

    erro render done

--- screens/Profil/index.tsx
+++ screens/Profil/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, StyleSheet, View, SafeAreaView, TextInput, Button, ScrollView, RefreshControl, Alert, Modal, TouchableOpacity, Pressable } from "react-native";
 import {GET_USER , UPDATE_USER , DELETE_USER} from "../../src/gql/UserGql";
 import { useMutation, useQuery } from "@apollo/client";
@@ -16,6 +16,10 @@ export default function Profil({ navigation }: { navigation: any }) {
     const [isPasswordVisible, setPasswordVisible] = useState(false);
     const [isConfirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
     const [passwordError, setPasswordError] = useState("");
+    const [pseudo, setPseudo] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [ConfirmPassword, setConfirmPassword] = useState("");
 
     const onRefresh = React.useCallback(() => {
       setRefreshing(true);
@@ -29,22 +33,17 @@ export default function Profil({ navigation }: { navigation: any }) {
       variables: { userId }, 
     });
 
-    if (error) {
-        return <Text>Erreur : {error.message}</Text>;
-    }
-  
-    if (loading) {
-    return <Text>Fetching data...</Text>;
-    }
-
-    const user = data.getUser as UserType;
-    const [pseudo, setPseudo] = useState(user.pseudo);
-    const [email, setEmail] = useState(user.email);
-    const [password, setPassword] = useState(user.password);
-    const [ConfirmPassword, setConfirmPassword] = useState("");
+    useEffect( () => {
+      if(data?.getUser){
+        const user = data.getUser as UserType;
+        setPseudo(user.pseudo);
+        setEmail(user.email);
+        setPassword(user.password);
+      }
+    }, [data]) 
 
     const [updateUser] = useMutation(UPDATE_USER, {
-        refetchQueries: [{ query: GET_USER, variables: { userId } }],
+       variables: { userId } ,
       });
 
     const [deleteUser] = useMutation(DELETE_USER, {
@@ -52,6 +51,25 @@ export default function Profil({ navigation }: { navigation: any }) {
     });
 
     const handleUpdateUser = async () => {
+        try {
+           await updateUser({
+            variables: {
+              userId: userId, 
+              pseudo: pseudo,
+              email: email,
+            },
+          });
+          
+        Alert.alert( "Mise à jour réussie",
+        "Vos informations ont été mises à jour avec succès.");
+
+        } catch (error) {
+          console.error('Erreur lors de la mise à jour de l\'utilisateur :', error);
+          Alert.alert("Erreur", "Une erreur est survenue. Veuillez réessayer plus tard.");
+        }
+      };
+      
+      const handleUpdatePasswordUser = async () => {
         try {
 
           if (password !== ConfirmPassword) {
@@ -62,9 +80,7 @@ export default function Profil({ navigation }: { navigation: any }) {
            await updateUser({
             variables: {
               userId: userId, 
-              pseudo: pseudo,
-              email: email,
-              password: password,
+              password: password
             },
           });
           
@@ -79,7 +95,7 @@ export default function Profil({ navigation }: { navigation: any }) {
           Alert.alert("Erreur", "Une erreur est survenue. Veuillez réessayer plus tard.");
         }
       };
-      
+
       const handleDeleteUser = async () => {
         try {
           await deleteUser({
@@ -100,122 +116,129 @@ export default function Profil({ navigation }: { navigation: any }) {
         }
       }
 
+    if (error) {
+        return <Text>Erreur : {error.message}</Text>;
+    }
+  
+    if (loading) { 
+      return <Text>Fetching data...</Text>;
+    }
     return (
-        <>
-            <ScrollView  
-                 refreshControl={
-                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-            }>
-                <View style={styles.container}>
-                    <Text style={styles.title}>Mon profil</Text>
-                    <View style={styles.card}>
-                        <Text style={styles.text}>Informations Personelles</Text>
-                        <SafeAreaView>
-                            <TextInput
-                                editable
-                                style={styles.input}
-                                value={pseudo}
-                                onChangeText={(text) => setPseudo(text)}
-                            />
-                            <TextInput
-                                style={styles.input}
-                                value={email}
-                                onChangeText={(text) => setEmail(text)}
-                            />
-                            <View style={styles.button}>
-                                <Button title="Enregistrer" color="#7ED957" onPress={handleUpdateUser}></Button>
-                            </View>
-                        </SafeAreaView>   
+      <>
+        <ScrollView  
+          refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          <View style={styles.container}>
+            <Text style={styles.title}>Mon profil</Text>
+              <View style={styles.card}>
+                <Text style={styles.text}>Informations Personelles</Text>
+                  <SafeAreaView>
+                    <TextInput
+                        editable
+                        style={styles.input}
+                        value={pseudo}
+                        onChangeText={(text) => setPseudo(text)}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
+                    />
+                    <View style={styles.button}>
+                        <Button title="Enregistrer" color="#7ED957" onPress={handleUpdateUser}></Button>
                     </View>
-
-                    <View style={styles.card}>
-                        <Text style={styles.text}>Mot de passe</Text>
-                        <SafeAreaView>
-                        <View style={styles.centeredView}>       
-                            <Modal
-                                animationType="slide"
-                                transparent={true}
-                                visible={modalVisible}
-                                onRequestClose={() => {
-                                Alert.alert('Modal has been closed.');
-                                setModalVisible(!modalVisible);
-                                }}>
-                                <View style={styles.centeredView}>
-                                    <View style={styles.modalView}>
-                                        <Text style={styles.text}>Modification du mot de passe</Text>
-                                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                            <TextInput
-                                                placeholder="Mot de passe actuelle"
-                                                style={{...styles.input, width:300}}
-                                                secureTextEntry={!isPasswordVisible}
-                                                value={password}
-                                                onChangeText={(text) => setPassword(text)}
-                                            />
-                                            <TouchableOpacity onPress={() => setPasswordVisible(!isPasswordVisible)}>
-                                            <Text>{isPasswordVisible ? <Ionicons size={25} name="eye-off-outline"  ></Ionicons> : <Ionicons size={25} name="eye-outline"></Ionicons>}</Text>
-                                            </TouchableOpacity>
-                                        </View>
-                                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                            <TextInput
-                                                placeholder="Confirme mot de passe"
-                                                style={{...styles.input, width:300}}
-                                                secureTextEntry={!isConfirmPasswordVisible}
-                                                value={ConfirmPassword}
-                                                onChangeText={(text) => setConfirmPassword(text)}
-                                            />
-                                            <TouchableOpacity onPress={() => setConfirmPasswordVisible(!isConfirmPasswordVisible)}>
-                                            <Text>{isConfirmPasswordVisible ? <Ionicons size={25} name="eye-off-outline"  ></Ionicons> : <Ionicons size={25} name="eye-outline"></Ionicons>}</Text>
-                                            </TouchableOpacity>
-                                        </View>
-                                        {passwordError ? (
-                                          <Text style={styles.textError}>{passwordError}</Text>
-                                        ) : null}
-                                        <View style={styles.button}>
-                                            <Button title="Enregistrer" color="#7ED957" onPress={handleUpdateUser}></Button>
-                                        </View>
-                                        <View style={{ ...styles.button, marginTop: 30 }}>
-                                            <Button  title="Close Update mot de passe" color="#7ED957" onPress={() => setModalVisible(!modalVisible)}></Button>
-                                        </View>
-                                    </View>
-                                </View>
-                            </Modal>
-
-                            <View style={styles.button}>
-                                <Button  title="Modifier mot de passe" color="#7ED957"  onPress={() => setModalVisible(true)}></Button>
-                            </View>
-                        </View>
-                        </SafeAreaView>   
-                    </View>
-
-
-                    <View style={styles.centeredView}>
-                      <Modal
-                        animationType="slide"
-                        transparent={true}
-                        visible={modalDeleteVisible}
-                        onRequestClose={() => {
-                          Alert.alert('Modal has been closed.');
-                        setModalDeleteVisible(!modalDeleteVisible);
-                        }}>
-                        <View style={styles.centeredView}>
-                          <View style={styles.modalView}>
-                            <Text style={styles.text}>Etes Vous sûr ?</Text>
-                            <Text>La suppression est définitive.</Text>
-                            <View style={{ marginTop: 30 }}>
-                              <Button title="Supprimer mon compte" color="red" onPress={handleDeleteUser}/>
-                            </View>
-                            <View style={{ ...styles.button, marginTop: 30 }}>
-                              <Button  title="Close" color="#7ED957" onPress={() => setModalDeleteVisible(!modalDeleteVisible)}/>
-                            </View>
+                  </SafeAreaView>   
+              </View>
+                  {/* MODAL UPDATE PASSWORD */}
+              <View style={styles.card}>
+                <Text style={styles.text}>Mot de passe</Text>
+                <SafeAreaView>
+                  <View style={styles.centeredView}>       
+                    <Modal
+                      animationType="slide"
+                      transparent={true}
+                      visible={modalVisible}
+                      onRequestClose={() => {
+                      Alert.alert('Modal has been closed.');
+                      setModalVisible(!modalVisible);
+                      }}
+                    >
+                      <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                          <Text style={styles.text}>Modification du mot de passe</Text>
+                          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <TextInput
+                                placeholder="Mot de passe actuelle"
+                                style={{...styles.input, width:300}}
+                                secureTextEntry={!isPasswordVisible}
+                                value={password}
+                                onChangeText={(text) => setPassword(text)}
+                            />
+                            <TouchableOpacity onPress={() => setPasswordVisible(!isPasswordVisible)}>
+                            <Text><Ionicons size={25} name={isPasswordVisible ? "eye-off-outline" : "eye-outline"}></Ionicons></Text>
+                            </TouchableOpacity>
+                          </View>
+                          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                              <TextInput
+                                  placeholder="Confirme mot de passe"
+                                  style={{...styles.input, width:300}}
+                                  secureTextEntry={!isConfirmPasswordVisible}
+                                  value={ConfirmPassword}
+                                  onChangeText={(text) => setConfirmPassword(text)}
+                              />
+                              <TouchableOpacity onPress={() => setConfirmPasswordVisible(!isConfirmPasswordVisible)}>
+                              <Text><Ionicons size={25} name={isConfirmPasswordVisible ? "eye-off-outline" : "eye-outline"}></Ionicons></Text>
+                              </TouchableOpacity>
+                          </View>
+                          {passwordError ? (
+                          <Text style={styles.textError}>{passwordError}</Text>
+                            ) : null}
+                          <View style={styles.button}>
+                              <Button title="Enregistrer" color="#7ED957" onPress={handleUpdatePasswordUser}></Button>
+                          </View>
+                          <View style={{ ...styles.button, marginTop: 30 }}>
+                              <Button  title="Close Update mot de passe" color="#7ED957" onPress={() => setModalVisible(!modalVisible)}></Button>
                           </View>
                         </View>
-                      </Modal>
-                      <Button title="Supprimer mon compte" color="red" onPress={() => setModalDeleteVisible(true)}/>
+                      </View>
+                    </Modal>
+                    <View style={styles.button}>
+                      <Button  title="Modifier mot de passe" color="#7ED957"  onPress={() => setModalVisible(true)}></Button>
                     </View>
-                 
-                </View>
-            </ScrollView>
-        </>
+                  </View>
+                </SafeAreaView>   
+              </View>
+              {/* MODAL DELETE */}
+              <View style={styles.centeredView}>
+                <Modal
+                  animationType="slide"
+                  transparent={true}
+                  visible={modalDeleteVisible}
+                  onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                  setModalDeleteVisible(!modalDeleteVisible);
+                  }}>
+                  <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                      <Text style={styles.text}>Etes Vous sûr ?</Text>
+                      <Text>La suppression est définitive.</Text>
+                      <View style={{ marginTop: 30 }}>
+                        <Button title="Supprimer mon compte" color="red" onPress={handleDeleteUser}/>
+                      </View>
+                      <View style={{ ...styles.button, marginTop: 30 }}>
+                        <Button  title="Close" color="#7ED957" onPress={() => setModalDeleteVisible(!modalDeleteVisible)}/>
+                      </View>
+                    </View>
+                  </View>
+                </Modal>
+                <Button title="Supprimer mon compte" color="red" onPress={() => setModalDeleteVisible(true)}/>
+              </View>
+              {/* FIN MODAL */}
+          </View>
+        </ScrollView>
+      </>
     );
 }
 
--- src/gql/UserGql.ts
+++ src/gql/UserGql.ts
@@ -11,7 +11,7 @@ export const CREATE_USER = gql`
 `;
 
 export const UPDATE_USER = gql`
-  mutation UpdateUser($userId: String!, $pseudo: String!, $email: String!, $password: String!) {
+  mutation UpdateUser($userId: String!, $pseudo: String, $email: String, $password: String) {
     updateUser(userId: $userId, pseudo: $pseudo, email: $email, password: $password) {
       id
       pseudo
